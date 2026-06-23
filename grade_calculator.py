cee6cc6da23436489aceecc0f7a2d01428f6333d
vaibhava krishna s k <vaibhavakrishna95@gmail.com>
2026-06-23 14:32:17 +0530

    committed  from b1

---
+++ grade_calculator.py
@@ -0,0 +1,12 @@
+def calculate_grade(marks): 
+ if marks >= 90:
+   return "A"
+    elif marks >= 75: 
+       return "B" 
+   elif marks >= 60: 
+       return "C" 
+else:
+           return "F"
+print(calculate_grade(85))
+
+
